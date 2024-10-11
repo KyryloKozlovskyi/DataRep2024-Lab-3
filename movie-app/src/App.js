1fb56d14fdff7d74f716f7cb0b5758ebc423ec5a
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-10-11 10:39:06 +0000

    Exercise 2 Task 1 Completed. Read and Create components created. Navigation bar updated.

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -5,16 +5,17 @@ import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
-
+import Read from './components/Read';
+import Create from './components/Create';
 function App() {
-  // Main page. Client side routing for home -> Content/read-> Footer/create -> Header pages.
+  // Main page. Client side routing.
   return (
     <Router>
       <NavigationBar />
       <Routes>
         <Route path="/home" element={<Content />} />
-        <Route path="/read" element={<Footer />} />
-        <Route path="/create" element={<Header />} />
+        <Route path="/read" element={<Read />} />
+        <Route path="/create" element={<Create />} />
       </Routes>
       <Footer />
     </Router>
